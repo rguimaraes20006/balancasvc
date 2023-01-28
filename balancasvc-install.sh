26602fdcdb02a85e83d94039aaaef87efba6576a
Rodrigo Guimarães <rguimaraes@ons.org.br>
2023-01-28 17:58:19 -0300

    Ajuste instalador script

--- balancasvc-install.sh
+++ balancasvc-install.sh
@@ -21,13 +21,7 @@ tar zxf  dotnet-sdk-6.0.404-linux-arm.tar.gz  -C .dotnet
 
 export PATH=$PATH:/root/.dotnet:root/.dotnet/tools
 
-
-git clone https://github.com/rguimaraes20006/balancasvc.git
-
-cd balancasvc
-
 dotnet publish -o /root/balancasvc/publish
-
 
 sudo useradd -m dotnetuser -p P@ssw0rd
 
